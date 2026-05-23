e1fb594b4bbaaf5b7e78fbe8501ce8a4adcd9cd1
chen <1198866090@qq.com>
2026-05-23 15:22:53 +0800

    Add test/error.js

---
+++ test/error.js
@@ -0,0 +1,18 @@
+// 错误语法测试（应该失败）
+
+// 缺少右括号
+function test1() {
+    var x = (1 + 2;
+}
+
+// 缺少右大括号
+function test2() {
+    var x = 10;
+    if (x > 5) {
+        console.log("test");
+    // 缺少 }
+
+// 不匹配的括号
+var arr = [1, 2, 3);
+var obj = { name: "test";
+
